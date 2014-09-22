9846678caa2a9e4c69f87aa1194d5f8e2e214fbf
kyak <bas@bmail.ru>
2014-09-22 16:51:01 +0400

    Add missing include

--- stellaris/rtiostreamserial.c
+++ stellaris/rtiostreamserial.c
@@ -2,6 +2,7 @@
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/fpu.h"
 #include "driverlib/gpio.h"
 #include "driverlib/rom.h"
 #include "driverlib/uart.h"
